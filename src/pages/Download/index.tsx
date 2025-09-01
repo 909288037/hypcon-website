5557aac1e0905957a4ed8377881d0632f2f57ee1
liushuai <909288037@qq.com>
2025-09-01 20:47:54 +0800

    feat: 选择类别按钮样式

--- src/pages/Download/index.tsx
+++ src/pages/Download/index.tsx
@@ -1,15 +1,16 @@
 import caretRightOutlined from '@/assets/images/caretRightOutlined.png';
 import caretUpOutlined from '@/assets/images/caretUpOutlined.png';
 import Header from '@/components/Header';
+import { CaretDownOutlined } from '@ant-design/icons';
 import { useClickAway } from 'ahooks';
 import classNames from 'classnames';
 import { useRef, useState } from 'react';
 import './index.less';
-import { CaretDownOutlined } from '@ant-design/icons';
 const Download = () => {
   // 选择类别
   const [selectType, setSelectType] = useState('选择类别');
   const [showType, setShowType] = useState(false);
+  const [subKeys, setSubKeys] = useState([]);
   const typeRef = useRef(null);
   useClickAway(() => {
     setShowType(false);
@@ -27,7 +28,7 @@ const Download = () => {
           <div className="fl-download-bg-search">
             {/* 选择类别 */}
             <div
-             ref={typeRef}
+              ref={typeRef}
               className={classNames(
                 'fl-download-bg-search-btn fl-download-bg-search-type',
                 {
@@ -48,36 +49,93 @@ const Download = () => {
                 </div>
               </div>
               <div className="fl-download-bg-search-list">
-                <div
-                  className="fl-download-bg-search-list-item"
-                  onClick={() => {
-                    setSelectType('边缘控制器');
-                    setShowType(false);
-                  }}
-                >
-                  边缘控制器
-                </div>
-                <div className="fl-download-bg-search-list-item">
-                  可编程控制系统
-                </div>
-                <div className="fl-download-bg-search-list-item">
-                  楼宇控制产品
-                </div>
-                <div className="fl-download-bg-search-list-item" onClick={(e) => {
-                    e.stopPropagation()
-                }}>传感器
-                    <span className='fl-download-bg-search-list-item-icon'><CaretDownOutlined /></span>
-                    <div className='fl-download-bg-search-list-item-sub'>
-                        {
-                            ['室内传感器', '外置传感器', '风管传感器', '水传感器',].map(item => {
-                                return (
-                                    <div className="fl-download-bg-search-list-item-sub-item">{item}</div>
-                                )
-                            })
+                {[
+                  {
+                    title: '边缘控制器',
+                  },
+                  {
+                    title: '可编程控制系统',
+                  },
+                  {
+                    title: '楼宇控制产品',
+                  },
+                  {
+                    title: '传感器',
+                    children: [
+                      {
+                        title: '室内传感器',
+                      },
+                      {
+                        title: '外置传感器',
+                      },
+                      {
+                        title: '风管传感器',
+                      },
+                      {
+                        title: '水传感器',
+                      },
+                    ],
+                  },
+                  {
+                    title: '软件产品',
+                  },
+                ].map((item) => {
+                  return (
+                    <div
+                      className={classNames('fl-download-bg-search-list-item', {
+                        'sub-show': subKeys.includes(item.title),
+                      })}
+                      key={item.title}
+                      onClick={(e) => {
+                        if (item.children) {
+                          e.stopPropagation();
+                          let newSubKeys = [...subKeys];
+                          if (newSubKeys.includes(item.title)) {
+                            newSubKeys = newSubKeys.filter(
+                              (key) => key !== item.title,
+                            );
+                          } else {
+                            newSubKeys.push(item.title);
+                          }
+                          setSubKeys(newSubKeys);
+                          return;
                         }
+                        setSelectType(item.title);
+                        setShowType(false);
+                      }}
+                    >
+                      {item.title}
+
+                      {item.children && (
+                        <>
+                          <span className="fl-download-bg-search-list-item-icon">
+                            <CaretDownOutlined />
+                          </span>
+                          <div
+                            className={classNames(
+                              'fl-download-bg-search-list-item-sub',
+                            )}
+                          >
+                            {item.children.map((sub) => {
+                              return (
+                                <div
+                                  className="fl-download-bg-search-list-item-sub-item"
+                                  key={sub.title}
+                                  onClick={() => {
+                                    setSelectType(sub.title);
+                                    setShowType(false);
+                                  }}
+                                >
+                                  {sub.title}
+                                </div>
+                              );
+                            })}
+                          </div>
+                        </>
+                      )}
                     </div>
-                </div>
-                <div className="fl-download-bg-search-list-item">软件产品</div>
+                  );
+                })}
               </div>
             </div>
             {/* 选择产品 */}
